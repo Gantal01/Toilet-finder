06572bbff71f86f4bcef280d3b7474cb557ee38e
Gertner Antal Gábor <h356676@stud.u-szeged.hu>
2025-11-10 22:32:47 +0100

    Admin felület tabok

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { HttpClientModule } from "@angular/common/http";
 import { NgForOf } from "@angular/common";
+import { MatTabGroup, MatTabLabel, MatTab } from "@angular/material/tabs";
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [HttpClientModule, NgForOf],
+  imports: [HttpClientModule, NgForOf, MatTabGroup, MatTab],
   providers: [ApiService],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
